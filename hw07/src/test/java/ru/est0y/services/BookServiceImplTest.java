91262070ae181513a05bec5a81be783f727334ca
est0y <asteoon@gmail.com>
2023-10-05 19:06:38 +0300

    refactor

--- hw07/src/test/java/ru/est0y/services/BookServiceImplTest.java
+++ hw07/src/test/java/ru/est0y/services/BookServiceImplTest.java
@@ -10,29 +10,31 @@ import org.springframework.context.annotation.Import;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Import({AuthorServiceImpl.class,GenreServiceImpl.class,BookServiceImpl.class})
+@Import({AuthorServiceImpl.class, GenreServiceImpl.class, BookServiceImpl.class})
 class BookServiceImplTest {
     @Autowired
     private BookServiceImpl bookService;
 
     @Autowired
     private TestEntityManager em;
+
     @Test
     void updateWithSameAuthorAndGenre() {
         SessionFactory sessionFactory = em.getEntityManager().getEntityManagerFactory()
                 .unwrap(SessionFactory.class);
         sessionFactory.getStatistics().setStatisticsEnabled(true);
-        bookService.update(1,"updated",1,1);
+        bookService.update(1, "updated", 1, 1);
         em.flush();
         assertThat(sessionFactory.getStatistics().getPrepareStatementCount()).isEqualTo(2);
         sessionFactory.getStatistics().clear();
     }
+
     @Test
     void updateWithOtherAuthorAndGenre() {
         SessionFactory sessionFactory = em.getEntityManager().getEntityManagerFactory()
                 .unwrap(SessionFactory.class);
         sessionFactory.getStatistics().setStatisticsEnabled(true);
-        bookService.update(1,"updated",2,2);
+        bookService.update(1, "updated", 2, 2);
         em.flush();
         assertThat(sessionFactory.getStatistics().getPrepareStatementCount()).isEqualTo(4);
         sessionFactory.getStatistics().clear();
