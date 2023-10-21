aadd956324283078413b4b8d9acd8fd30aa9ef6a
est0y <asteoon@gmail.com>
2023-10-21 17:45:29 +0300

    homework 09

---
+++ hw09/src/main/java/ru/est0y/services/stringifiers/BookCommentStringifier.java
@@ -0,0 +1,21 @@
+package ru.est0y.services.stringifiers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.est0y.domain.Book;
+import ru.est0y.domain.BookComment;
+
+@Service
+@RequiredArgsConstructor
+public class BookCommentStringifier implements Stringifier<BookComment> {
+
+    private final Stringifier<Book> bookStringifier;
+
+    @Override
+    public String stringify(BookComment comment) {
+        return String.format("id:%s, text:%s,book:(%s)",
+                comment.getId(),
+                comment.getText(),
+                bookStringifier.stringify(comment.getBook()));
+    }
+}
