4f6c0c322ea4b902bca81bd356581c0e11a15823
est0y <asteoon@gmail.com>
2023-11-30 12:53:12 +0300

    homework 15

---
+++ hw15/src/main/java/ru/est0y/domain/Butterfly.java
@@ -0,0 +1,8 @@
+package ru.est0y.domain;
+
+import lombok.Data;
+
+@Data
+public class Butterfly {
+    private final String name;
+}
---
+++ hw15/src/main/java/ru/est0y/domain/Caterpillar.java
@@ -0,0 +1,8 @@
+package ru.est0y.domain;
+
+import lombok.Data;
+
+@Data
+public class Caterpillar {
+    private final String name;
+}
